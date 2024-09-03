b8b97f8561420e4f244b46b2fc45c4a712150694
Space Dwarf <166604927+DRGSpaceDwarf@users.noreply.github.com>
2024-09-03 13:27:04 +0200

    Feat. Added new buttons

--- kalkulator/app.js
+++ kalkulator/app.js
@@ -1,23 +1,32 @@
-// Først henter vi ut alle elementene vi skal bruke fra HTML
 const txtTall1 = document.querySelector("#tall1")
 const txtTall2 = document.querySelector("#tall2")
 const btnPluss = document.querySelector("#pluss")
+const btnMinus = document.querySelector("#minus")
+const btnGange = document.querySelector("#gange")
+const btnDele = document.querySelector("#dele")
 const svarDiv = document.querySelector("#svar")
 
-btnPluss.onclick = function() {
+btnPluss.onclick = function () {
     let tall1 = Number(txtTall1.value);
     let tall2 = Number(txtTall2.value);
     let sum = tall1 + tall2
     svarDiv.innerHTML = sum
 }
-
-/*
-    En alternativ måte å skrive en funksjon er å lage en "arrow function", slik:
-        () => { }
-    Dette btyr det samme som å skrive
-        function() { }
-        men det er bare en slags kortere måte å lage en funksjon, f.eks:
-        btnpluss.onclick = () => {
-        ...
-        }
-*/
+btnMinus.onclick = function () {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 - tall2
+    svarDiv.innerHTML = sum
+}
+btnGange.onclick = function () {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 * tall2
+    svarDiv.innerHTML = sum
+}
+btnDele.onclick = function () {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 / tall2
+    svarDiv.innerHTML = sum
+}
